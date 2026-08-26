92ce8d1094d512a7df884cc7a6b8a7010ac8abd7
xiaokeai3 <73268791+xiaokeai3@users.noreply.github.com>
2026-08-26 10:18:11 +0800

    Update filogic.mk

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -1055,7 +1055,7 @@ define Device/fzs_p3
   BLOCKSIZE := 128k
   PAGESIZE := 2048
   KERNEL_IN_UBI := 1
-  DEVICE_PACKAGES := kmod-mt7981-firmware mt7981-wo-firmware kmod-usb3 luci-app-qmodem luci-app-fancontrol luci-app-oaf
+  DEVICE_PACKAGES := kmod-mt7981-firmware mt7981-wo-firmware
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += fzs_p3
